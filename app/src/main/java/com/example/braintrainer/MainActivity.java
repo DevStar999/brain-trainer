314cde9d22741ac91fd67b2728b3ee0b73061f2d
DevStar999 <wolverinerage99@gmail.com>
2021-06-21 18:36:01 +0530

    Completed implementation of to start game and move to empty Game Activity

--- app/src/main/java/com/example/braintrainer/MainActivity.java
+++ app/src/main/java/com/example/braintrainer/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.braintrainer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView infoContentTextView;
+
+    private void initialise() {
+        infoContentTextView = findViewById(R.id.infoContentTextView);
+
+        String textOfContentTextView =  "Solve as many questions as you\n" +
+                                        "can by choosing the answer of\n"+
+                                        "the question among it's four\n" +
+                                        "options in a time of 30 seconds,\n" +
+                                        "for all the questions, to score\n" +
+                                        "as high as possible. All the Best!";
+        infoContentTextView.setText(textOfContentTextView);
+    }
+
+    public void startGame(View view) {
+        Intent intent = new Intent(MainActivity.this, GameActivity.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initialise();
     }
 }
